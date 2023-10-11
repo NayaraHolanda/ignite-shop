4eb49e9a7fffcccf9c18116ccf44503398c1f610
Nayara Holanda <naayaraholanda@gmail.com>
2023-10-11 15:23:50 -0300

    Add home structure

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -8,19 +8,26 @@ export const {
   keyframes,
   getCssText,
   theme,
-  createTheme
+  createTheme,
 } = createStitches({
   theme: {
     colors: {
       white: '#fff',
-      
+
       gray900: '#121214',
       gray800: '#202024',
       gray300: '#c4c4cc',
       gray100: '#e1e1e6',
 
       green500: '#00875f',
-      green300: '#00b37e'
-    }
-  }
+      green300: '#00b37e',
+    },
+
+    fontSizes: {
+      md: '1.125rem',
+      lg: '1.25rem',
+      xl: '1.5rem',
+      '2xl': '2rem',
+    },
+  },
 })
