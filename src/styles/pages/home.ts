74214ef5fd99fef3c6fa3eeed425681e00c3e51f
Nayara Holanda <naayaraholanda@gmail.com>
2023-10-24 09:35:07 -0300

    Update home.ts

--- src/styles/pages/home.ts
+++ src/styles/pages/home.ts
@@ -6,6 +6,7 @@ export const HomeContainer = styled('main', {
   width: '100%',
   maxWidth: 'calc(100vw - ((100vw - 1180px) / 2))',
   marginLeft: 'auto',
+  height: 'calc(100vh - 52px - 8rem)',
   maxHeight: 656,
 })
 
